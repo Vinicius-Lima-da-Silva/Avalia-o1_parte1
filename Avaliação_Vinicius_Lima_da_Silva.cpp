72a6f99c0596144079f9509d631075c0f1d1bd80
Vinicius Lima <vini_lima98@outlook.com>
2023-10-23 22:08:04 -0300

    Interface médico

--- Avaliação_Vinicius_Lima_da_Silva.cpp
+++ Avaliação_Vinicius_Lima_da_Silva.cpp
@@ -188,3 +188,113 @@ public:
         } while (opcao != 0);
     }
 };
+class Interface_Medico {
+    public:
+    int opcao;
+    vector<Medico*> medicos;
+
+    int localizaCrm(vector<Medico*> medicos, string crm) {
+        int i = 0;
+        for (auto el : medicos) {
+            if (el->getCrm() == crm) {
+                return i;
+            }
+            i++;
+        }
+        return -1;
+    }
+
+    void menu() {
+        do {
+            cout << "--Cadastro de Medicos--" << endl;
+            cout << "1. Incluir" << endl;
+            cout << "2. Excluir (Inserir CRM)" << endl;
+            cout << "3. Alterar (Inserir CRM)" << endl;
+            cout << "4. Listar" << endl;
+            cout << "5. Localizar (Inserir CRM)" << endl;
+            cout << "0. Voltar" << endl;
+            cin >> opcao;
+
+            string auxNome, auxCrm, auxEspecialidade;
+
+            if (opcao == 1) {
+                cout << "Informe o nome: ";
+                getline(cin >> ws, auxNome);
+
+                cout << "Informe o CRM: ";
+                getline(cin >> ws, auxCrm);
+
+                cout << "Informe a Especialidade: ";
+                getline(cin >> ws, auxEspecialidade);
+
+                Medico* medico = new Medico(auxCrm, auxEspecialidade, auxNome);
+
+                medicos.push_back(medico);
+            } else if (opcao == 2) {
+                cout << "Informe o CRM do médico que deseja excluir: ";
+                getline(cin >> ws, auxCrm);
+                int indice = localizaCrm(medicos, auxCrm);
+                if (indice != -1) {
+                    delete medicos[indice]; // Liberar memória do médico excluído
+                    medicos.erase(medicos.begin() + indice);
+                    cout << "Cadastro de médico excluído com sucesso!" << endl;
+                } else {
+                    cout << "Médico com CRM " << auxCrm << " não encontrado." << endl;
+                }
+            } else if (opcao == 3) {
+    cout << "Informe o CRM do médico que deseja alterar: ";
+    getline(cin >> ws, auxCrm);
+    int indice = localizaCrm(medicos, auxCrm);
+    
+    if (indice != -1) {
+        cout << "Médico encontrado. O que você deseja alterar?" << endl;
+        cout << "1. Nome" << endl;
+        cout << "2. Especialidade" << endl;
+        cout << "Escolha uma opção (1/2): ";
+        int escolha;
+        cin >> escolha;
+        
+        if (escolha == 1) {
+            cout << "Informe o novo nome: ";
+            getline(cin >> ws, auxNome);
+            medicos[indice]->setNome(auxNome);
+            cout << "Nome do médico atualizado com sucesso!" << endl;
+        } else if (escolha == 2) {
+            cout << "Informe a nova Especialidade: ";
+            getline(cin >> ws, auxEspecialidade);
+            medicos[indice]->setEspecialidade(auxEspecialidade);
+            cout << "Especialidade do médico atualizada com sucesso!" << endl;
+        } else {
+            cout << "Opção inválida. Nenhum dado foi alterado." << endl;
+        }
+    } else {
+        cout << "Médico com CRM " << auxCrm << " não encontrado. Nenhum dado foi alterado." << endl;
+    }
+} else if (opcao == 4) {
+                for (auto el : medicos) {
+                    cout << "Nome: " << el->getNome() << endl;
+                    cout << "CRM: " << el->getCrm() << endl;
+                    cout << "Especialidade: " << el->getEspecialidade() << endl;
+                    cout << endl;
+                }
+            } else if (opcao == 5) {
+                cout << "Informe o CRM que deseja localizar: ";
+                getline(cin >> ws, auxCrm);
+                int indice = localizaCrm(medicos, auxCrm);
+                if (indice != -1) {
+                    cout << "Médico encontrado:" << endl;
+                    cout << "Nome: " << medicos[indice]->getNome() << endl;
+                    cout << "CRM: " << medicos[indice]->getCrm() << endl;
+                    cout << "Especialidade: " << medicos[indice]->getEspecialidade() << endl;
+                } else {
+                    cout << "Médico com CRM " << auxCrm << " não encontrado." << endl;
+                }
+            } else if (opcao == 0) {
+                return;
+            } else {
+                cout << "OPCAO INVALIDA!" << endl;
+            }
+        } while (opcao != 0);
+    }
+
+};
